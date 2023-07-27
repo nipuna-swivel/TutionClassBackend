1f2487f6f73438113d5d3833824b42153b89ff95
nipuna-swivel <nipuna@swivelgroup.com.au>
2023-07-27 23:15:17 +0530

    adding auth modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthService } from './auth/auth.service';
 import { AuthController } from './auth/auth.controller';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule],
   controllers: [AppController, AuthController],
   providers: [AppService, AuthService],
 })
